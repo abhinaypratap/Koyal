78ec752de5a2c7d7ae4d033bc6f85481d9be8974
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-30 20:57:27 +0530

    populate table view from database

--- Koyal/Controllers/EventViewController.swift
+++ Koyal/Controllers/EventViewController.swift
@@ -10,20 +10,29 @@ import UIKit
 class EventViewController: UIViewController {
 
     @IBOutlet weak var eventTableView: UITableView!
+    private let manager = EventManager()
+    var events = [Event]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Events"
+        navigationController?.navigationBar.prefersLargeTitles = true
 //        eventTableView.clearsSelectionOnViewWillAppear = false
         let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         debugPrint(path[0])
-//        let event = Event(context: PersistentStorage.shared.context)
-//        event.id = UUID()
-//        event.category = "Fun"
-//        event.content = "Had fun learning CoreData framework"
-//        event.time = Date()
-//
-//        PersistentStorage.shared.saveContext()
+        
+        eventTableView.delegate = self
+        eventTableView.dataSource = self
+        populateTable()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        populateTable()
+    }
 }
 
 
@@ -45,9 +54,39 @@ extension EventViewController {
 //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 //        <#code#>
 //    }
-//    
+//
 //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        <#code#>
 //    }
 //}
 
+extension EventViewController {
+    func populateTable() {
+        events = manager.fetchEvent()
+        DispatchQueue.main.async {
+            self.eventTableView.reloadData()
+        }
+    }
+}
+
+extension EventViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("didSelectRowAt")
+    }
+}
+
+extension EventViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = eventTableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
+        let event = events[indexPath.row]
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM YYYY hh:mm a"
+        cell.timeLabel.text = dateFormatter.string(from: event.time!)
+        cell.contentLabel.text = event.content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        events.count
+    }
+}
