6984a17c8ddbe2a087f1945eaf3086e9e8fe5f5c
Abhinay Pratap <abhinaypratap@icloud.com>
2022-07-30 20:34:23 +0530

    manager functions

--- Koyal/Managers/EventManager.swift
+++ Koyal/Managers/EventManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import CoreData
+
+struct EventManager {
+    
+    private let _eventDataRepository = EventDataRepository()
+    
+    func createEvent(event: Event) {
+        _eventDataRepository.create(event: event)
+    }
+    
+    func fetchEvent() -> [Event] {
+        _eventDataRepository.getAll()
+    }
+    
+    func fetchEvent(byIdentifier id: UUID) -> Event? {
+        _eventDataRepository.get(byIdentifier: id)
+    }
+    
+    func updateEvent(event: Event) -> Bool {
+        _eventDataRepository.update(event: event)
+    }
+    
+    func deleteEvent(byIdentifier id: UUID) -> Bool {
+        _eventDataRepository.delete(byIdentifier: id)
+    }
+}
+
